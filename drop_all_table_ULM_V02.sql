22e8a829aa19f28a0565f0adbc0f585f575c132e
rochatheigvd <yannis.rochat@hes-so.ch>
2025-03-21 13:40:16 +0100

    Create drop_all_table_ULM_V02.sql

---
+++ drop_all_table_ULM_V02.sql
@@ -0,0 +1,15 @@
+drop table prescription;
+drop table patient_rdv_historique;
+drop table rdv;
+drop table patient;
+drop table medecin;
+drop table hopital;
+drop table specialisation;
+drop table medicament;
+drop table personne;
+drop table adresse;
+drop table assurance;
+drop type type_sexe;
+drop type type_medicament;
+drop type type_motif_rdv;
+drop type type_specialisation;
