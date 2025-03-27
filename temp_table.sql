89595c1bf6f5750122cd9eeedd2a31041f0f3143
loannjui <loann.juillerat@heig-vd.ch>
2025-03-27 13:47:32 +0100

    Update temp_table.sql

--- temp_table.sql
+++ temp_table.sql
@@ -1,5 +1,5 @@
 -- Patient
-create temp table patient(
+create temp table temp_patient(
 id integer primary key,
 nom varchar(50),
 prenom varchar(50),
@@ -10,12 +10,12 @@ assurance varchar,
 sexe varchar
 );
 
-copy patient(id, nom, prenom, date_naiss, adresse, telephone, assurance, sexe)
+copy temp_patient(id, nom, prenom, date_naiss, adresse, telephone, assurance, sexe)
 from 'csv\patient.csv'
 WITH CSV HEADER;
 
 --Médecin 
-create temp table medecin(
+create temp table temp_medecin(
 id integer primary key,
 nom varchar(50),
 prenom varchar(50),
@@ -25,12 +25,12 @@ sexe varchar,
 adresse_hopital varchar()
 );
 
-copy medecin(id, nom, prenom, specialite, telephone, sexe, adresse_hopital)
+copy temp_medecin(id, nom, prenom, specialite, telephone, sexe, adresse_hopital)
 from 'csv\medecins.csv'
 WITH CSV HEADER;
 
 -- Rendez-vous
-create temp table rdv(
+create temp table temp_rdv(
 id integer primary key,
 patient_id integer references patient(id),
 medecin_id integer references medecin(id),
@@ -38,30 +38,30 @@ rdv_date date,
 motif varchar
 );
 
-copy rdv(id, patient_id, medecin_id, rdv_date)
+copy temp_rdv(id, patient_id, medecin_id, rdv_date)
 from 'csv\rdv.csv'
 WITH CSV HEADER;
 
 -- Médicament
-create temp table medicament(
+create temp table temp_medicament(
 id integer primary key,
 nom varchar,
 dosage varchar,
 med_type varchar
 )
 
-copy medicament(id, nom, dosage, med_type)
+copy temp_medicament(id, nom, dosage, med_type)
 from 'csv\medoc.csv'
 WITH CSV HEADER;
 
 -- Prescription
-create temp table prescription(
+create temp table temp_prescription(
 id integer primary key,
 rdv_id integer,
 medicament_id integer,
 duree integer
 );
 
-copy prescription(id, rdv_id, medicament_id, duree)
+copy temp_prescription(id, rdv_id, medicament_id, duree)
 from 'csv\prescriptions.csv'
 WITH CSV HEADER;
