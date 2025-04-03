f8031aec88a27a2a4c6f7f7ececfd9cf5272266d
lucaleone15 <lucaleone2004@gmail.com>
2025-04-03 11:52:50 +0200

    2

---
+++ test_import2.sql
@@ -0,0 +1,25 @@
+-- Active: 1743080815125@@127.0.0.1@5432@hopital
+create temp table temp_medicament(
+id integer,
+nom varchar,
+dosage varchar,
+med_type varchar
+);
+
+copy temp_medicament(id, nom, dosage, med_type)
+from '/private/tmp/medoc.csv'
+WITH CSV HEADER
+DELIMITER ';';
+
+create type type_medicament as enum ('comprime',
+'gelule',
+'aerosol',
+'injection');
+create table medicament(id serial primary key,
+nom varchar(30) not null,
+dosage varchar(10) not null,
+med_type type_medicament not null);
+
+INSERT INTO medicament (nom, dosage, med_type)
+SELECT nom, dosage, med_type::type_medicament FROM temp_medicament;
+
