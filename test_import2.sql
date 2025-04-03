8190d403b61d88e6d75d5a2f97e529cf2295c754
lucaleone15 <lucaleone2004@gmail.com>
2025-04-03 13:03:21 +0200

    Update test_import2.sql

--- test_import2.sql
+++ test_import2.sql
@@ -1,5 +1,7 @@
 -- Active: 1743080815125@@127.0.0.1@5432@hopital
 
+-- Enlever les anciennes tables --
+
 DROP TABLE medicament;
 
 DROP TYPE type_medicament;
@@ -7,6 +9,7 @@ DROP TYPE type_medicament;
 DROP TABLE temp_medicament;
 
 --POUR LES MÉDICAMENTS--
+-- CRÉATION DE LA TABLE TEMPORAIRE --
 
 create temp table temp_medicament(
 id integer,
@@ -15,10 +18,15 @@ dosage varchar,
 med_type varchar
 );
 
+-- IMPORT DES DONNÉES DANS TABLE TEMPORAIRE --
+
+
 copy temp_medicament(id, nom, dosage, med_type)
 from '/private/tmp/medoc.csv'
 WITH CSV HEADER
 DELIMITER ';';
+
+-- CRÉATION DU TYPE ENUM --
 
 create type type_medicament as enum ('Comprimé',
 'Gélule',
@@ -26,14 +34,20 @@ create type type_medicament as enum ('Comprimé',
 'Injection', 
 'Capsule');
 
+-- CRÉATION DE LA TABLE FINALE --
+
+
 create table medicament(id serial primary key,
 nom varchar(30) not null,
 dosage varchar(10) not null,
 med_type type_medicament not null);
 
+-- IMPORT DANS LA TABLE FINALE --
+
 INSERT INTO medicament (id, nom, dosage, med_type)
 SELECT id, nom, dosage, med_type::type_medicament FROM temp_medicament;
 
+-- TEST --
 
 SELECT *
 FROM medicament
