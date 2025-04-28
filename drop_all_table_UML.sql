80533f8b2109e26056a85cec56faa7338b4cf0c3
lucaleone15 <lucaleone2004@gmail.com>
2025-04-28 15:28:33 +0200

    test

--- drop_all_table_UML.sql
+++ drop_all_table_UML.sql
@@ -1,3 +1,4 @@
+-- Active: 1743080815125@@127.0.0.1@5432@hopital
 drop table prescription;
 drop table patient_rdv_historique;
 drop table rdv;
@@ -13,3 +14,9 @@ drop type type_sexe;
 drop type type_medicament;
 drop type type_motif_rdv;
 drop type type_specialisation;
+
+DROP TABLE temp_patient;
+DROP TABLE temp_medecin;
+DROP TABLE temp_rdv;
+DROP TABLE temp_medicament;
+DROP TABLE temp_prescription;
